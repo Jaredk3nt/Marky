91f1b47701982e386100f957b5ad0126f09ab9fa
Jared Jones <jkjones.dev@gmail.com>
2018-01-11 22:03:11 -0600

    Marky working w/o tables or code blocks

---
+++ marky.js
@@ -0,0 +1,219 @@
+'use strict';
+(function () {
+    const lib = {
+        newline: /^\n+/,
+        heading: /^(#{1,6})\s*([^\n]+)/, //
+        blockquote: /^>([^\n]+)/,
+        ulist: /^([*+-])\s*([^\n]+)(?:\n|$)/,
+        link: /^\[([^\n]+)\]\(([^\n]+)\)/,
+        img: /^!\[([^\n]+)\]\(([^\n]+)\)/,
+        bold: /^[*_]{2}([^\n]+)[*_]{2}/,
+        italic: /^[*_]([^\n]+)[*_]/,
+        text: /([^\n]+)/
+    }
+
+    function tokenize(md) {
+        let tokens = [],
+            token;
+
+        while (md) {
+            // Grab newlines
+            if (token = lib.newline.exec(md)) {
+                tokens.push({
+                    rule: 'newline',
+                });
+                md = md.substring(token[0].length);
+
+                continue;
+            }
+            // Grab headings (#...######)
+            if (token = lib.heading.exec(md)) {
+                tokens.push({
+                    rule: 'heading',
+                    rank: token[1].length,
+                    text: token[2]
+                });
+                md = md.substring(token[0].length);
+                continue;
+            }
+            // Grab blockquotes
+            if (token = lib.blockquote.exec(md)) {
+                tokens.push({
+                    rule: 'blockquote',
+                    text: token[1]
+                });
+                md = md.substring(token[0].length);
+                continue;
+            }
+            // Grab unordered list
+            if (token = lib.ulist.exec(md)) {
+                let list = [];
+                while (token = lib.ulist.exec(md)) {
+                    list.push(token[2]);
+                    md = md.substring(token[0].length);
+                }
+                tokens.push({
+                    rule: 'ulist',
+                    list: list
+                });
+                continue;
+            }
+            // Grab free text
+            if (token = lib.text.exec(md)) {
+                tokens.push({
+                    rule: 'text',
+                    text: token[1]
+                })
+                md = md.substring(token[0].length);
+                continue;
+            }
+        }
+
+        return tokens;
+    }
+
+    function parseInner(text) {
+        let str = '',
+            htmlifier = new Htmlifier(),
+            token;
+        while (text) {
+            // Catch inline links
+            if (token = lib.link.exec(text)) {
+                let link = {
+                    rule: 'link',
+                    text: token[1],
+                    url: token[2]
+                }
+                text = text.substring(token[0].length);
+                str += htmlifier.link(link);
+                continue;
+            }
+            // Catch inline bolding
+            if (token = lib.bold.exec(text)) {
+                let t = {
+                    rule: 'bold',
+                    text: token[1]
+                }
+                text = text.substring(token[0].length);
+                str += htmlifier.strong(t);
+                continue;
+            }
+            // Catch image links
+            if (token = lib.img.exec(text)) {
+                let image = {
+                    rule: 'img',
+                    text: token[1],
+                    src: token[2]
+                }
+                text = text.substring(token[0].length);
+                str += htmlifier.img(image);
+            }
+            // Catch any lines that begin with a [, *, or _ that were not caught by the rules above
+            if (token = /(^[^\[\*_]+)/.exec(text)) {
+                str += token[1];
+                text = text.substring(token[0].length);
+                continue;
+            }
+            // Catch remaining
+            str += text;
+            text = text.substring(text.length);
+        }
+        return str;
+    }
+
+    // Object that takes in tokens and produces the correct HTML as a string
+    function Htmlifier() {
+        //setup
+    }
+
+    Htmlifier.prototype.heading = function (heading) {
+        return '<h' + heading.rank + '>' +
+            parseInner(heading.text) +
+            '</h' + heading.rank + '>';
+    }
+
+    Htmlifier.prototype.blockquote = function (quote) {
+        return '<blockquote>' +
+            parseInner(quote.text) +
+            '</blockquote>';
+    }
+
+    Htmlifier.prototype.ulist = function (list) {
+        return '<ul>' +
+            list.list.reduce((result, current) => {
+                return result + '<li>' + parseInner(current) + '</li>';
+            }, '') +
+            '</ul>';
+    }
+
+    Htmlifier.prototype.link = function (link) {
+        return '<a href="' +
+            link.url + '">' +
+            parseInner(link.text) +
+            '</a>';
+    }
+
+    Htmlifier.prototype.img = function (img) {
+        return '<img src="' + img.src + '" ' +
+            'alt="' + img.text + '">';
+    }
+
+    Htmlifier.prototype.strong = function (text) {
+        return '<strong>' +
+            parseInner(text.text) +
+            '</strong>';
+    }
+
+    Htmlifier.prototype.paragraph = function (text) {
+        return '<p>' +
+            parseInner(text.text) +
+            '</p>'
+    }
+
+    function htmlify(tokens, htmlifier) {
+        let str = "";
+        for (let token of tokens) {
+            switch (token.rule) {
+                case 'heading':
+                    {
+                        str += htmlifier.heading(token);
+                        break;
+                    }
+                case 'blockquote':
+                    {
+                        str += htmlifier.blockquote(token);
+                        break;
+                    }
+                case 'ulist':
+                    {
+                        str += htmlifier.ulist(token);
+                        break;
+                    }
+                case 'text':
+                    {
+                        str += htmlifier.paragraph(token);
+                        break;
+                    }
+            }
+        }
+        return str;
+    }
+
+    function marky(md) {
+        const htmlifier = new Htmlifier();
+        let tokens = tokenize(md);
+        return htmlify(tokens, htmlifier);
+    }
+
+    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+        module.exports = marky;
+    } else {
+        if (typeof define === 'function' && define.amd) {
+            define([], function () {
+                return marky;
+            });
+        } else {
+            window.marky = marky;
+        }
+    }
+})();
